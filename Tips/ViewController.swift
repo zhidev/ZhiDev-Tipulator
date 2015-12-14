e110a236542d554ed86c017b15743a61bdc0d92a
Douglas <Doug@douglass-macbook-air.local>
2015-12-14 01:06:15 -0800

    Initial Commit

---
+++ Tips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tips
+//
+//  Created by Douglas on 12/14/15.
+//  Copyright © 2015 Dougli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
