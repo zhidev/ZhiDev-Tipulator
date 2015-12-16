2650dd92eec913287241aa4332f2618f7a2fed08
zhidev <Doug@douglass-macbook-air.local>
2015-12-15 17:44:24 -0800

    Added Changing color themes, Added some foot notes in case the need to pretty things up or expand on functionality

--- SettingsViewController.swift
+++ SettingsViewController.swift
@@ -24,9 +24,10 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     }
 
     /* Settings dictionary (for expansion) */
-    /*var sections = [
-        "Choose Default Tip or Customize Your Own"
-    ]*/
+    var sections:[String] = [
+        "Choose Default Tip or Customize Your Own, or change color schemes"
+        
+    ]
     
     /* Dictionary for our table cells*/
     var tipDictionary: [[String : AnyObject]] = [
@@ -50,10 +51,17 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
             [Keys.Name: "Tip #1", Keys.Description: tip1],
             [Keys.Name: "Tip #2", Keys.Description: tip2],
             [Keys.Name: "Tip #3", Keys.Description: tip3],
-            [Keys.Name: "Custom", Keys.Description: "Click here to customize a tip value"]
+            [Keys.Name: "Custom", Keys.Description: "Click here to customize a tip value"],
+            [Keys.Name: "Light colored scheme", Keys.Description: "Blue / White colors"],
+            [Keys.Name: "Dark colored scheme", Keys.Description: "Black / White"]
         ]
     }
     
+    override func viewWillAppear(animated: Bool) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let colors = defaults.integerForKey("colors")
+        themeChange(colors)
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 
@@ -70,17 +78,17 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
     }
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Choose Default Tip or Customize Your Own"
+        //return "Choose Default Tip or Customize Your Own"
+        return self.sections[section]
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //# of rows
-        
         return tipDictionary.count
         
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return sections.count
     }
 
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
@@ -93,7 +101,7 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
             defaults.synchronize()
 
         }
-        else{
+        else if(indexPath.row == 3){
             /* We still follow normal logic and set int to 3 so we know if user set a custom value */
             let defaults = NSUserDefaults.standardUserDefaults()
             defaults.setInteger(3, forKey: "selected")
@@ -101,8 +109,18 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
             
             /*Segway to 3rd VC */
             performSegueWithIdentifier("cusSeg", sender: self)
-
-            
+        }
+        else if(indexPath.row == 4){
+            let defaults = NSUserDefaults.standardUserDefaults()
+            defaults.setInteger(0, forKey: "colors")
+            defaults.synchronize()
+            themeChange(0)
+        }
+        else if(indexPath.row == 5){
+            let defaults = NSUserDefaults.standardUserDefaults()
+            defaults.setInteger(1, forKey: "colors")
+            defaults.synchronize()
+            themeChange(1)
         }
     }
     
@@ -128,6 +146,18 @@ class SettingsViewController: UIViewController, UITableViewDataSource, UITableVi
             print("else block called, nothing fancy should happen")
         }
         self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func themeChange(value: Int){
+        if(value == 0){
+            self.view.backgroundColor = UIColor.cyanColor()
+        }
+        else if (value == 1){
+            self.view.backgroundColor = UIColor.blackColor()
+
+        }
+    
+
     }
 
 }
